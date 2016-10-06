f6aade107353a5289c375e25e0345306654406e1
Madison Kerndt <madison.kerndt@gmail.com>
2016-10-06 03:29:02 -0600

    location and styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,12 +5,30 @@ import { Link, Router, IndexRoute, Route, browserHistory } from 'react-router';
 
 export default class App extends Component {
 
+  handleLocation() {
+    const geolocation = require('geolocation');
+    const geo = navigator.geolocation;
+    let lat;
+    let lon;
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+      console.log(position.coords.latitude, position.coords.longitude);
+    });
+  }
 
   render() {
     return (
       <section>
-        <Link to="/"><h1>Weather Tracker</h1></Link>
-        <Link to="/extended-forecast"><nav>Extended Forecast</nav></Link>
+        <header>
+          <Link to="/" className="nav-title"><h1>Weather Tracker</h1></Link>
+          <nav>
+            <article className="nav-current-forcast">
+              <p>Current forcast paceholder text for Denver blue skies</p>
+            </article>
+            <Link className="link-extended-city-forcast" to="/extended-forecast"><nav>Extended Forecast »</nav></Link>
+          </nav>
+          <button onClick={() => this.handleLocation()}>Return Location</button>
+        </header>
         <div>{this.props.children}</div>
       </section>
     )
