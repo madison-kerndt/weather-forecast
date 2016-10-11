c537021e44da1758dcdf54cead5f363f20ded66a
Ben Godfrey <bcgodfrey91@gmail.com>
2016-10-10 22:27:14 -0600

    Add action and reducer tests

--- src/actions/action.test.js
+++ src/actions/action.test.js
@@ -1,12 +1,32 @@
-import { localWeather } from './index';
+import { localWeather, extendedForecast, pinNewCity, CURRENT_WEATHER, EXTENDED_FORECAST, ADD_PINNEDCITY  } from './index';
+
 
 describe('actions', () => {
    it('should create an action that identifies local weather', () => {
-     const weather = 'RAIN';
+     const weather = 'Rain';
      const expectedAction = {
-       type: CURRENT_WEATHER,
+       type: 'CURRENT_WEATHER',
        weather
      };
-     expect(currentLocalCity(weather)).toEqual(expectedAction);
+     expect(localWeather(weather)).toEqual(expectedAction);
    });
+
+
+   it('should create an action that identifies an exteneded forecast', () => {
+     const weather = 'Rain';
+     const expectedAction = {
+       type: 'EXTENDED_FORECAST',
+       weather
+     };
+     expect(extendedForecast(weather)).toEqual(expectedAction);
    });
+
+   it('should create an action that pins a new city', () => {
+     const input = '80202';
+     const expectedAction = {
+       type: 'ADD_PINNEDCITY',
+       input
+     };
+     expect(pinNewCity(input)).toEqual(expectedAction);
+   });
+ });
